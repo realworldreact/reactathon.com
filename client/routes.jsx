5809a343efc61b57b045b704bb60d821c793a985
Berkeley Martinez <Berkeley@robotie.com>
2016-10-20 01:14:36 -0700

    Feat(static): use plain routes version of static sites

--- client/routes.jsx
+++ client/routes.jsx
@@ -1,16 +1,10 @@
-import React from 'react';
-import { IndexRoute, Route } from 'react-router';
-
 import App from './components/App.jsx';
 import Home from './routes/home/Home.jsx';
 import childRoutes from './routes/index.jsx';
 
-export default (
-  <Route
-    component={ App }
-    path='/'
-    >
-    <IndexRoute component={ Home } />
-    { childRoutes }
-  </Route>
-);
+export default {
+  component: App,
+  path: '/',
+  indexRoute: { component: Home },
+  childRoutes
+};
--- client/routes/index.jsx
+++ client/routes/index.jsx
@@ -1,13 +1,8 @@
-import React from 'react';
-import { Route } from 'react-router';
-
 import About from './about/About.jsx';
 
-export default (
-  <div>
-    <Route
-      component={ About }
-      path='about'
-    />
-  </div>
-);
+export default [
+  {
+    component: About,
+    path: 'about'
+  }
+];
