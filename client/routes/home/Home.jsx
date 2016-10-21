b29f826e237c5fa0405f76d4e122f57b7b9adad0
Berkeley Martinez <Berkeley@robotie.com>
2016-10-21 02:45:15 -0700

    feat(styl): add flex to home page

--- client/routes/home/Home.jsx
+++ client/routes/home/Home.jsx
@@ -9,14 +9,29 @@ const propTypes = {};
 export default class Home extends Component {
   render() {
     return (
-      <div className={ cx('home', 'col-sm-6', 'col-sm-offset-3') }>
+      <section className={ cx('home') }>
         <header>
-          <h3>Home</h3>
+          <h1>Immortan</h1>
+          <h2>You will ride eternal, shiny, and chrome</h2>
         </header>
         <article>
-          Text
+          <p>
+            Bicycle rights kitsch activated charcoal venmo
+            banh mi, stumptown cronut 3 wolf moon hexagon
+            wayfarers poke tousled. Fashion axe selvage pour-over
+            ramps heirloom, occupy cardigan pug art party.
+            Poke mustache blog, seitan activated charcoal
+            ugh sriracha hella mlkshk put a bird on it before
+            they sold out 90's austin distillery.
+            Plaid pickled pitchfork mustache lomo chambray
+            fingerstache XOXO hot chicken glossier snackwave,
+            drinking vinegar green juice. Cray af hoodie street art.
+            Readymade 90's mixtape, lomo four dollar toast kitsch leggings. Hot
+            chicken ethical pabst, fam forage lyft kale chips 3 wolf moon lo-fi
+            slow-carb.
+          </p>
         </article>
-      </div>
+      </section>
     );
   }
 }
