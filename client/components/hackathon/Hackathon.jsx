7fd60a7165cdb5a7363670d684b5d659218ecefe
Berkeley Martinez <Berkeley@robotie.com>
2017-02-22 16:26:18 -0800

    feat(app): Add more GA events fix(lint): fix lint in several files

--- client/components/hackathon/Hackathon.jsx
+++ client/components/hackathon/Hackathon.jsx
@@ -1,42 +1,34 @@
 import React, { PropTypes, PureComponent } from 'react';
+import { connect } from 'react-redux';
 import classnames from 'classnames/bind';
-import isEmail from 'validator/lib/isEmail';
-import { reduxForm } from 'redux-form';
 import ActionButton from '../Action-Button.jsx';
 import github from '../../images/sponsors/github-sponsor.png';
 
 import styles from './hackathon.styl';
 
 import TitleCard from '../Title-Card.jsx';
-import {
-  DOMOnlyProps,
-  createFormValidator,
-  makeFieldRequired
-} from '../../form-utils.js';
 import { trackEvent } from '../../redux/index.js';
 
 const cx = classnames.bind(styles);
 const volunteerUrl = 'https://docs.google.com/forms/d/e/' +
   '1FAIpQLSdkgVtFTUggBhnXZwK6RYhXUJhB0ainAxR31XEwkFVO8bBu3Q/viewform';
-const propTypes = {
-  fields: PropTypes.object,
-  invalid: PropTypes.bool,
-  isSubmitting: PropTypes.bool,
-  submitToUpdates: PropTypes.func.isRequired
-};
-const hiddenName = 'b_61b17cbde66a062a953c16339_c14aaf8666';
 
-const validators = {
-  email: makeFieldRequired(isEmail),
-  hidden: (value) => !value
+const propTypes = {
+  clickOnRegister: PropTypes.func.isRequired,
+  clickOnVolunteer: PropTypes.func.isRequired
 };
 
 function mapDispatchToProps(dispatch) {
   const dispatchers = {
-    submitToUpdates: () => dispatch(trackEvent({
+    clickOnRegister: () => dispatch(trackEvent({
       category: 'Hackathon',
-      action: 'submit',
-      label: 'user subscribes to hackathon updates'
+      action: 'click',
+      label: 'user clicks on Register navigation button'
+    })),
+    clickOnVolunteer: () => dispatch(trackEvent({
+      category: 'Hackathon',
+      action: 'click',
+      label: 'user clicks on Volunteer form link'
     }))
   };
   // redux-form does not respect mapDispatchFactory
@@ -48,10 +40,8 @@ function mapDispatchToProps(dispatch) {
 export class Hackathon extends PureComponent {
   render() {
     const {
-      invalid = true,
-      isSubmitting,
-      fields: { hidden, email },
-      submitToUpdates
+      clickOnRegister,
+      clickOnVolunteer
     } = this.props;
     return (
       <div className={ cx('hackathon') }>
@@ -80,7 +70,10 @@ export class Hackathon extends PureComponent {
               Developers, Designers, and Entrepreneurs Welcome
             </h3>
             <div>
-              <ActionButton href='#Register'>
+              <ActionButton
+                href='#Register'
+                onClick={ clickOnRegister }
+                >
                 Register Free
               </ActionButton>
               <p>
@@ -91,6 +84,7 @@ export class Hackathon extends PureComponent {
                 <span>{ ' ' }</span>
                 <a
                   href={ volunteerUrl }
+                  onClick={ clickOnVolunteer }
                   target='_blank'
                   >
                   Go here
@@ -114,12 +108,7 @@ export class Hackathon extends PureComponent {
 
 Hackathon.displayName = 'Hackathon';
 Hackathon.propTypes = propTypes;
-export default reduxForm(
-  {
-    form: 'mail-chimp',
-    validate: createFormValidator(validators),
-    fields: Object.keys(validators)
-  },
+export default connect(
   null,
   mapDispatchToProps
 )(Hackathon);
